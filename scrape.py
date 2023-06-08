d3bec8e5b7bf972f3ab1911de06612024bfd5178
mur1chan <muerchen@hotmail.com>
2023-06-08 12:50:12 +0200

    added parenting

--- scrape.py
+++ scrape.py
@@ -2,17 +2,30 @@ import undetected_chromedriver.v2 as uc
 from selenium.webdriver import Chrome
 import time
 
-class Selenium:
+class Browser:
     def __init__(self):
         self.driver = None
 
-    def start_browser(self):
+    def start(self):
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def get_url(self, url):
+        if not self.driver:
+            raise Exception("Browser not started")
+        self.driver.get(url)
+
+class SeleniumBrowser(Browser):
+    def __init__(self, product):
+        super().__init__()
+        self.product = product
+
+    def start(self):
         self.driver = uc.Chrome(executable_path=r'drivers/chromedriver.exe', version_main=97)
-        self.driver.get("https://google.de")
-        time.sleep(99999)
 
-# Erstelle eine Instanz von uc.Chrome
+    def get_product_url(self):
+        self.get_url(f"https://www.kleinanzeigen.de/s-{self.product}/k0")
 
-
-selenium = Selenium()
-selenium.start_browser()
+# Erstelle eine Instanz von SeleniumBrowser
+browser = SeleniumBrowser(input("Wonach möchten Sie suchen: "))
+browser.start()
+browser.get_product_url()
