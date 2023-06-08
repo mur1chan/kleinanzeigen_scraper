2213f34ff9ecf78f6ad4951fa43a812d9ab30403
mur1chan <muerchen@hotmail.com>
2023-06-08 12:40:56 +0200

    undetected chromedriver against bot detection

--- scrape.py
+++ scrape.py
@@ -1,5 +1,18 @@
-class Selenium:
-    def __init__(self, driver):
-        pass
+import undetected_chromedriver.v2 as uc
+from selenium.webdriver import Chrome
+import time
 
-    def start_browser()
+class Selenium:
+    def __init__(self):
+        self.driver = None
+
+    def start_browser(self):
+        self.driver = uc.Chrome(executable_path=r'drivers/chromedriver.exe', version_main=97)
+        self.driver.get("https://google.de")
+        time.sleep(99999)
+
+# Erstelle eine Instanz von uc.Chrome
+
+
+selenium = Selenium()
+selenium.start_browser()
